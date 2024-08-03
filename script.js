102b857b12dcc79bbbc0c7576a989aab4186516a
dindakhoiria <dindakhoiria15@gmail.com>
2024-08-03 20:52:13 +0700

    Initial commit

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Toggle between dark and light theme
 document.getElementById('theme-toggle').addEventListener('click', () => {
     document.body.classList.toggle('light-theme');
+});
+
+// Toggle the visibility of the menu on mobile
+document.getElementById('menu-toggle').addEventListener('click', () => {
+    const menu = document.querySelector('nav ul');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 });
